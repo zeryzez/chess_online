1f14ab93f9246f5b46057e1685e82a6f39bf8479
zeryzez <96591252+zeryzez@users.noreply.github.com>
2025-05-08 22:07:39 +0200

    Create console_view.py

---
+++ view/console_view.py
@@ -0,0 +1,18 @@
+class ConsoleView:
+    """
+    Vue console pour l'affichage ASCII du plateau et la saisie des coups.
+    """
+    @staticmethod
+    def render_board(board):
+        """Affiche le plateau en ASCII avec bordures."""
+        print(board.unicode(borders=True))
+
+    @staticmethod
+    def prompt_move():
+        """Invite l'utilisateur à entrer un coup en SAN."""
+        return input("🔸 Ton coup : ").strip()
+
+    @staticmethod
+    def show_message(message):
+        """Affiche un message quelconque à l'utilisateur."""
+        print(message)
